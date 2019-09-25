d0ed784e01c77437413ef14007b80e893ecc9648
ombima452 <christineombima452@gmail>
2019-09-25 11:39:36 +0300

    create route for addpost

--- __init__.py
+++ __init__.py
@@ -47,21 +47,21 @@ def login():
 
 
 
-# @app.route('/addpost',methods=['POST'])
-# def addpost():
+@app.route('/addpost',methods=['POST'])
+def addpost():
      
-#     title = request.form['title']
-#     subtitle = request.form['subtitle']
-#     author = request.form['author']
-#     content = request.form['content']
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
         
-#     post = Blogpost(title = title,subtitle = subtitle, author = author,content = content,date_posted = datetime.now())
+    post = Blogpost(title = title,subtitle = subtitle, author = author,content = content,date_posted = datetime.now())
     
-#     # db.session.add(post)
-#     # db.session.commit()
+    # db.session.add(post)
+    # db.session.commit()
     
-#     return redirect(url_for('post'))
+    return redirect(url_for('post'))
     
     
-# if __name__== '__main__':
-#     app.run(debug=True) 
+if __name__== '__main__':
+    app.run(debug=True) 
